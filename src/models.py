919de86b1624151ce87529e24e062f7eee6099c9
jbara55 <barahona.jorge@gmail.com>
2021-09-05 03:34:38 +0000

    final

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,31 +8,77 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Personajes(Base):
+    __tablename__ = 'Personajes'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    uid = Column(Integer, primary_key=True)
+    height = Column(Integer)
+    mass = Column(Integer)
+    gender = Column(String)
+    name = Column(String(50))
+    homeworld = Column(String(50), ForeignKey('Planet.name'))
+ 
+class Planet(Base):
+    __tablename__ = 'Planet'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter =  Column(Integer)
+    poulation = Column(Integer)
+    climate = Column(String)
+    name = Column(String)
+    relacionPersonaje= relationship("Personajes")
+    
+class Planet_vehicle(Base):
+    __tablename__ = 'Planet_vehicle'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    idPlanet = Column(Integer,primary_key=True)
+    manufacturaV = Column(String)
+    
 
+class Personaje_Vehiculo(Base):
+    __tablename__ = 'Personaje_Vehiculo'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    piloto_uid = Column(Integer, primary_key=True)
+    uid_name = Column(String)
+  
+
+class ListaFavoritos(Base):
+    __tablename__ = 'ListaFavoritos'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    mail_usuario = Column(String)
+    favoritoPersonaje = Column(Integer)
+    favoritoPlaneta = Column(Integer)
+    favoritoVehiculo = Column(String)
+    
+class Vehicles(Base):
+    __tablename__ = 'Vehicles'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    name = Column(String,primary_key=True)
+    model = Column(String)
+    vehicle_class = Column(String)
+    lenght = Column(Float)
+    manufactura = Column(String)
+   
+
+class Usuario(Base):
+    __tablename__ = 'Usuario'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    nombre = Column(String, primary_key=True)
+    mail = Column(String)
+    Password = Column(String)
+    
+    
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+render_er(Base, 'diagram.png')
